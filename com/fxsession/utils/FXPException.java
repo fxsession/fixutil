b8e65fe56c0ad5ee05505ec475093df81b54d7c5
fxsession <dmitry.vulf@gmail.com>
2013-10-24 13:01:03 +0400

    1.0

--- com/fxsession/utils/FXPException.java
+++ com/fxsession/utils/FXPException.java
@@ -1,6 +1,5 @@
 package com.fxsession.utils;
 
-
 /**
  * @author Dmitry Vulf
  * 
--- com/fxsession/utils/FXPXml.java
+++ com/fxsession/utils/FXPXml.java
@@ -4,7 +4,6 @@ package com.fxsession.utils;
  * Singleton 
  */
 
-
 import java.io.File;
 import java.io.IOException;
 
